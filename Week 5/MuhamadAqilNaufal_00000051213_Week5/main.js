043afa3806615f3f14c2bb67b0e2d3d2680f9919
Muhamad Aqil Naufal <Naulynaufal@gmail.com>
2022-02-22 11:40:48 +0700

    first commit

---
+++ Week 5/MuhamadAqilNaufal_00000051213_Week5/main.js
@@ -0,0 +1,3 @@
+document.getElementById("greetings").innerHTML = "<h2>Selamat datang di Universitas Multimedia Nusantara, Kampus Lokal dengan Kualitas Internasional<h2>";
+
+document.getElementsByTagName("img")[1].src = "gambar/jurusan_si.jpg";
